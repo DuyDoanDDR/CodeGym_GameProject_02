141904fc9e3c2c888939b556e1a52ba02fb110f1
Doan Minh Duy <duydoan812@gmail.com>
2025-07-01 23:04:58 +0700

    02/07/2025_Comitted

--- CodeGym_GameProject_02/Assets/Scripts/GameManager.cs
+++ CodeGym_GameProject_02/Assets/Scripts/GameManager.cs
@@ -1,34 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    public static bool gameOver = false;
+    public static bool gameOver;
+    //public GameObject gameOverUI;
+    
+
+    public float gameTime;
+    public static bool gameWin;
+
+
+    public bool gameIsStopped => gameOver || gameWin;
+
+    public static GameManager instance;
+   
+   
+
 
     // Start is called before the first frame update
     void Start()
     {
+        gameOver = false;
+        gameWin = false;
         
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameOver)
+        if (gameOver || gameWin)
         {
             return;
         }
         if (PlayerStats.lives <= 0)
         {
-            GameOver();
+            
+            GameStatement( gameOver, UIManager.instance.gameOverUI);
+        }
+        else
+        {
+            gameTime -= Time.deltaTime;
+            if (gameTime <= 0)
+            {
+                GameStatement( gameWin, UIManager.instance.gameWinUI);
+            }
         }
     }
 
-    void GameOver()
+
+    void GameStatement( bool gameStatement, GameObject statementUI)
     {
-        gameOver = true;
-        Debug.Log("GameOver");
+        gameStatement = true;
         
+        UIManager.instance.GameStatementUI(statementUI);
+        
+        Time.timeScale = 0.0f;
     }
+   
 }
--- CodeGym_GameProject_02/Assets/Scripts/UIManager.cs
+++ CodeGym_GameProject_02/Assets/Scripts/UIManager.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI waveTimer;
     public TMPro.TextMeshProUGUI money;
     public TMPro.TextMeshProUGUI HP;
+    public GameObject gameOverUI;
+    public GameObject gameWinUI;
+    public GameObject restartButton;
+    public GameObject quitButton;
+
+    public static UIManager instance;
+
     WaveSpawner waveSpawner;
 
-   
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
         waveSpawner = WaveSpawner.instance;
+       gameManager = GameManager.instance;
     }
 
     public void WaveTimerUI()
@@ -30,6 +44,40 @@ public class UIManager : MonoBehaviour
     public void HPUI()
     {
         HP.text = string.Format("HP: " + PlayerStats.lives);
+    }
+
+
+    public void GameStatementUI(GameObject statementUI)
+    {
+        if (statementUI == null)
+        {
+            Debug.LogError("statementUI  null");
+            return;
+        }
+        statementUI.SetActive(true);       
+    }
+    public void GameOverUI()
+    {
+        GameStatementUI(gameOverUI);
+    }
+    public void GameWinUI()
+    {
+        GameStatementUI(gameWinUI);
+    }
+    public void RestartButton()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //if (gameManager.gameIsStopped)
+        //{
+        //    GameManager.gameWin = false;
+        //    GameManager.gameOver = false;
+        //}
+
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
     }
     // Update is called once per frame
     void Update()
